f0920ee970ccf17f06b3ae7d0e3794aaa2ac1bcf
Med Khaled Boutabba <medkhaled.boutabba@gmail.com>
2022-08-13 12:59:27 +0100

    Add the Navbar component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div>
+      <Navbar />
       <h1> Hello </h1>
       <button className="btn btn-success"> Ok </button>
     </div>
-    
-      
   );
 }
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// C:\Users\acer\projects\portfolio\node_modules\bootstrap\dist\css\bootstrap.min.css 
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 import './index.css';
