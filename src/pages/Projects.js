b8661f515d21f36bce98c4708c9d092ff6597c60
Med Khaled Boutabba <medkhaled.boutabba@gmail.com>
2022-08-14 13:34:49 +0100

    display skills

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -20,7 +20,7 @@ function Pojects() {
   return (
     <div>
       {PROJECTS.map(item => (
-        <div key={item.githubId} style={{ border: 'solid 1px black', margin: 10, padding: 10 }}>
+        <div key={item.githubId} style={{ border: 'solid 1px black', margin: 10}}>
           <h1> {item.title} </h1>
           <p>{item.description}</p>
           <a href={item.url} target='_blank'>Preview</a>
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function Skils() {
+  const [skills, setSkills] = useState([])
+  useEffect(() => {
+    axios.get('https://devmastery-assets.vercel.app/technologies.json')
+      .then((response) => { setSkills(response.data) })
+      .catch((error) => { console.log(error) })
+  }, [])
   return (
-    <div>Skills</div>
+    <div className='row'>
+      {skills.map(item => (
+        <div key={item.Id} className='col-12 col-sm-6 col-md-4 col-lg-3 p-3'>
+          <div class="card" >
+            <img src={item.image} class="card-img-top" alt={`${item.name} logo`} />
+            <div class="card-body">
+              <h5 class="card-title">{item.name}</h5>
+              {/* <p class="card-text"> {item.description}</p> */}
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+
+            </div>
+          </div>
+
+        </div>
+      ))}
+
+    </div>
   )
 }
 
