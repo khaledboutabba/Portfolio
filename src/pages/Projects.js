376f89bd47311110d211a8cbc833f5b1ff62134e
Med Khaled Boutabba <medkhaled.boutabba@gmail.com>
2022-08-14 12:23:16 +0100

    Add content to the project page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,8 +1,33 @@
 import React from 'react'
 
+const PROJECTS = [
+  {
+    title: 'pricing',
+    description: 'Pricing section with 3 plans',
+    url: 'https://khaledboutabba.github.io/pricing/',
+    githubId: 'pricing',
+  },
+  {
+    title: 'business land',
+    description: 'Web page developed with only HTML and CSS',
+    url: 'https://khaledboutabba.github.io/business-land',
+    githubId: 'business-land',
+  },
+
+]
+
 function Pojects() {
   return (
-    <div>Pojects</div>
+    <div>
+      {PROJECTS.map(item => (
+        <div key={item.githubId} style={{ border: 'solid 1px black', margin: 10, padding: 10 }}>
+          <h1> {item.title} </h1>
+          <p>{item.description}</p>
+          <a href={item.url} target='_blank'>Preview</a>
+          <h6> {item.githubId} </h6>
+        </div>
+      ))}
+    </div>
   )
 }
 
