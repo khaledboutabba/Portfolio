451f13ee642a8bc176b9ee7d88bd3d865f7cfc9c
Med Khaled Boutabba <medkhaled.boutabba@gmail.com>
2022-08-20 10:07:14 +0100

    add contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,22 @@
 import React from 'react'
 
 function Contact() {
+  const contactInformation = {
+email:'medkhaled.boutabba@gmail.com',
+phoneNumber : '+216 26 345 900',
+adress:{
+  city:'Sfax',
+  country:'Tunisia',
+  street:'Airport road',
+  postalCode:3064,
+}
+}
   return (
-    <div>Contact</div>
+    <div>
+    <p> <i class="bi bi-envelope"></i> {contactInformation.email} </p>
+    <p> <i class="bi bi-phone"></i> {contactInformation.phoneNumber} </p>
+    <p> <i class="bi bi-geo-alt"></i> {contactInformation.adress.street}; {contactInformation.adress.postalCode} {contactInformation.adress.city}; {contactInformation.adress.country} </p>
+    </div>
   )
 }
 
